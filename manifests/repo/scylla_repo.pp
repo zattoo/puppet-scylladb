9cba244c813e8ae543be36ffe5f75959a49520bd
naveen-goswami <naveen.goswami@zattoo.com>
2019-09-15 19:40:19 +0200

    fix identation

--- manifests/repo/scylla_repo.pp
+++ manifests/repo/scylla_repo.pp
@@ -16,9 +16,9 @@ class scylla::repo::scylla_repo (
         include apt
         include apt::update
         apt::key {'scylla':
-         id     => $key_id,
-         source => $key_url,
-         options => 'http-proxy="http://squid.zattoo.com:3128 "',
+          id     => $key_id,
+          source => $key_url,
+          options => 'http-proxy="http://squid.zattoo.com:3128 "',
         } ~>
         exec { "recieve_scylla_gpg_key":
           path      => '/bin:/usr/bin:/sbin:/usr/sbin',
@@ -28,16 +28,16 @@ class scylla::repo::scylla_repo (
         }
 
         apt::source { 'scylla.source.https.list':
-           location => "https://repositories.scylladb.com/scylla/downloads/scylladb/b956f642-36ba-4ba7-a565-68df8f10acb5/scylla/deb/debian/scylladb-3.0",
-           release  => $::os['distro']['codename'],
-           repos    => 'non-free',
-           notify   => Exec['apt_update'],
+          location => "https://repositories.scylladb.com/scylla/downloads/scylladb/b956f642-36ba-4ba7-a565-68df8f10acb5/scylla/deb/debian/scylladb-3.0",
+          release  => $::os['distro']['codename'],
+          repos    => 'non-free',
+          notify   => Exec['apt_update'],
         }
         apt::source { 'scylla.source.http.list':
-           location => "http://download.opensuse.org/repositories/home:/scylladb:/scylla-3rdparty-stretch/Debian_9.0/",
-           release  => './',
-           repos    => '',
-           notify   => Exec['apt_update'],
+          location => "http://download.opensuse.org/repositories/home:/scylladb:/scylla-3rdparty-stretch/Debian_9.0/",
+          release  => './',
+          repos    => '',
+          notify   => Exec['apt_update'],
 
         }
       }
